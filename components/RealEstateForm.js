176e15cfb3bae4f5a8cad10f1c63cd4d0140ce80
Hermann Hairet <hermannhairet44@gmail.com>
2024-04-07 23:20:54 +0200

    add another steps in form

--- components/RealEstateForm.js
+++ components/RealEstateForm.js
@@ -7,12 +7,12 @@ const RealEstateForm = ({ resetForm }) => {
     const [formContent, setFormContent] = useState('initial');
 
     const onFormSubmit = data => {
+        console.log(data);
         if (Array.isArray(data.propertyType)) {
             setFormContent(data.propertyType[0]);
         } else {
             setFormContent(Object.keys(data)[0]);
         }
-        console.log(formContent);
         reset();
     };
 
@@ -195,7 +195,7 @@ const RealEstateForm = ({ resetForm }) => {
                     <h1 className='text-2xl font-semibold font-sans'>Votre budget: </h1>
                     <form onSubmit={handleSubmit(onFormSubmit)} className="mt-8 max-w-sm">
                         <div>
-                            <label htmlFor="number-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">minimum:</label>
+                            <label htmlFor="number-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">La fourchette basse:</label>
                             <input
                                 {...register("budgetMin")}
                                 type="number"
@@ -207,7 +207,7 @@ const RealEstateForm = ({ resetForm }) => {
                             />
                         </div>
                         <div>
-                            <label htmlFor="number-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">maximum:</label>
+                            <label htmlFor="number-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">La fourchette haute:</label>
                             <input
                                 {...register("bugdetMax")}
                                 type="number"
@@ -229,7 +229,93 @@ const RealEstateForm = ({ resetForm }) => {
 
 
             )}
-        </div>
+            {(formContent.slice(0, 6) === 'budget') && (
+                <>
+                    <h1 className='text-2xl font-semibold font-sans'>En cas de coup de coeur: </h1>
+                    <form onSubmit={handleSubmit(onFormSubmit)} className="mt-8 max-w-sm">
+                        <div>
+                            <input
+                                {...register("coupDeCoeur")}
+                                type="number"
+                                id="number-input"
+                                aria-describedby="helper-text-explanation"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="?????? €"
+                                required
+                            />
+                        </div>
+                        <button
+                            type="submit"
+                            className="py-2.5 px-5 mt-3 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                        >
+                            Soumettre
+                        </button>
+                    </form>
+                </>
+            )}
+            {(formContent === 'coupDeCoeur') && (
+                <>
+                    <form onSubmit={handleSubmit(onFormSubmit)} className="mt-8 max-w-sm">
+                        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> Il se déclenche comment le coup de foudre?</label>
+                        <textarea
+                            {...register("coupDeFoudre")}
+                            id="message"
+                            rows="4"
+                            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Lâchez vous ici...">
+                        </textarea>
+                        <button
+                            type="submit"
+                            className="py-2.5 px-5 mt-3 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                        >
+                            Soumettre
+                        </button>
+                    </form>
+                </>
+            )}
+            {(formContent === 'coupDeFoudre') && (
+                <>
+                    <h1 className='text-2xl font-semibold font-sans'>Les travaux: </h1>
+                    <form onSubmit={handleSubmit(onFormSubmit)} className="mt-8 max-w-sm">
+                        <label htmlFor="minmax-range" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Partant ou pas partant</label>
+                        <input
+                            {...register("travaux")}
+                            id="minmax-range"
+                            type="range"
+                            min="0"
+                            max="10"
+                            defaultValue="5"
+                            className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+                        />
+                        <button
+                            type="submit"
+                            className="py-2.5 px-5 mt-3 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                        >
+                            Soumettre
+                        </button>
+                    </form>
+                </>
+            )}
+            {formContent === 'travaux' && (
+                <>
+                    <h1 className='text-2xl font-semibold font-sans'>Inclus dans le budget ou pas?</h1>
+                    <form onSubmit={handleSubmit(onFormSubmit)} className="mt-8">
+                        <div className="flex items-center mb-2 mt-4">
+                            <input {...register("travauxDansBudget")} id="travauxDansBudget-checkbox" type="checkbox" value="travauxDansBudget" className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                            <label htmlFor="travauxDansBudget-checkbox" className="ml-2 text-lg text-gray-900 dark:text-gray-300">Oui</label>
+                        </div>
+                        <div className="flex items-center mb-2 mt-4">
+                            <input {...register("travauxPasDansBudget")} id="travauxPasDansBudget" type="checkbox" value="travauxPasDansBudget" className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                            <label htmlFor="travauxPasDansBudget-checkbox" className="ml-2 text-lg text-gray-900 dark:text-gray-300">Non</label>
+                        </div>
+                        <button type="submit" className="py-2.5 px-5 mt-3 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Soumettre</button>
+                    </form>
+                </>
+            )
+            }
+
+        </div >
+
 
     );
 };
