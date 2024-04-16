26b9cbe4ec953f496698f1e5b31d7186ac4f9146
Hermann Hairet <hermannhairet44@gmail.com>
2024-04-16 18:39:27 +0200

    fix esLint for prod

--- next.config.js
+++ next.config.js
@@ -10,7 +10,11 @@ const nextConfig = {
       'www.flaticon.com',
     ],
   },
-  ignoreDuringBuilds: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
